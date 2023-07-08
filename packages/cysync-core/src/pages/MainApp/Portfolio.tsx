6c8d513f4bdb24ecd6972123b803549830066419
Advaith <advaith1601@gmail.com>
2023-07-08 13:47:00 +0530

    add account

--- packages/cysync-core/src/pages/MainApp/Portfolio.tsx
+++ packages/cysync-core/src/pages/MainApp/Portfolio.tsx
@@ -1,14 +1,16 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import {
-  ConnectionStatusType,
   SyncStatusType,
+  ConnectionStatusType,
   Topbar,
+  Container,
+  ConfettiBlast,
+  SuccessDialog,
 } from '@cypherock/cysync-ui';
 import React, { FC, useState } from 'react';
-
-import { AssetAllocation } from '~/pages/MainApp/Components/AssetAllocation';
 import { selectLanguage, useAppSelector } from '~/store';
+import { DeviceNotConnectedDialogBox } from '../OnBoarding';
 
 export const Portfolio: FC = () => {
   const lang = useAppSelector(selectLanguage);
@@ -29,7 +31,25 @@ export const Portfolio: FC = () => {
         syncStatus={syncState}
         connectionStatus={connectionState}
       />
-      <AssetAllocation />
+
+      <Container height="screen" $bgColor="sideBar" display="flex">
+        {/* <ConfettiBlast />
+        <SuccessDialog
+          title={'Account added successfully'}
+          subtext={'Add other accounts or return to portfolio'}
+          headerText={'Add coin/account'}
+          buttonText={'Done'}
+          handleClick={() => console.log('hello')}
+          secButtonText={'Add more'}
+          handleSecClick={() => console.log('test')}
+        /> */}
+
+        <DeviceNotConnectedDialogBox
+          headerText={'Add coin/Account'}
+          title={lang.strings.onboarding.deviceDetection.title}
+        
+        />
+      </Container>
     </>
   );
 };
--- packages/ui/src/components/molecules/Dialog/SuccessDialog.tsx
+++ packages/ui/src/components/molecules/Dialog/SuccessDialog.tsx
@@ -1,27 +1,46 @@
 import React from 'react';
 
-import { DialogBox, DialogBoxBody, DialogBoxFooter } from './DialogBox';
+import {
+  DialogBox,
+  DialogBoxBody,
+  DialogBoxFooter,
+  DialogBoxHeader,
+} from './DialogBox';
 
 import { successIcon } from '../../../assets/images';
 import { Typography, LangDisplay, Image, Container, Button } from '../../atoms';
 import { AlertBox } from '../AlertBox';
 
 export interface SuccessDialogProps {
+  headerText?: string;
   title: string;
   subtext?: string;
   buttonText?: string;
+  secButtonText?: string;
   alertText?: string;
   handleClick?: () => void;
+  handleSecClick?: () => void;
 }
 
 export const SuccessDialog: React.FC<SuccessDialogProps> = ({
+  headerText,
   title,
   subtext,
   buttonText,
+  secButtonText,
   alertText,
   handleClick,
+  handleSecClick,
 }) => (
   <DialogBox width={500}>
+    {headerText && (
+      <DialogBoxHeader>
+        <Typography variant="h6" $textAlign="center" color="muted">
+          <LangDisplay text={headerText} />
+        </Typography>
+      </DialogBoxHeader>
+    )}
+
     <DialogBoxBody>
       <Image src={successIcon} alt="Success Icon" />
       <Container display="flex" direction="column" gap={4}>
@@ -38,6 +57,12 @@ export const SuccessDialog: React.FC<SuccessDialogProps> = ({
     </DialogBoxBody>
     {buttonText && handleClick && (
       <DialogBoxFooter height={101}>
+        {secButtonText && handleSecClick && (
+          <Button variant="secondary" onClick={handleSecClick}>
+            <LangDisplay text={secButtonText} />
+          </Button>
+        )}
+
         <Button onClick={handleClick} variant="primary">
           <LangDisplay text={buttonText} />
         </Button>
